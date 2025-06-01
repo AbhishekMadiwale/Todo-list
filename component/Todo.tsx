c39edefa21cb3a7a8fa7ba01bc93776417e92426
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-06-01 13:16:38 +0530

    storing list in local storage and saving the tasks on key press

--- component/Todo.tsx
+++ component/Todo.tsx
@@ -7,19 +7,24 @@ type TodoProp = {
     text: string;
     completed: boolean;
   };
-  completeTodo : (id: number) => void;
+  completeTodo: (id: number) => void;
+  deleteTodo: (id: number) => void; 
 };
 
-function Todo({ todo, completeTodo } : TodoProp) {
+function Todo({ todo, completeTodo, deleteTodo }: TodoProp) {
   return (
     <div className="bg-gray-700 text-white font-bold p-5 my-5 rounded-xl flex justify-between items-center">
-      <p>{todo.text}</p>
+      <p className={todo.completed === true ? "line-through" : " "}>
+        {todo.text}
+      </p>
       <div className="flex gap-10 items-center cursor-pointer">
-        <FaCheckCircle 
-          className="hover:text-gray-500" 
+        <FaCheckCircle
+          className="hover:text-green-800"
           onClick={() => completeTodo(todo.id)}
-          />
-        <FaTrash className="hover:text-gray-500" />
+        />
+        <FaTrash className="hover:text-red-600"
+          onClick={() => deleteTodo(todo.id)}
+        />
       </div>
     </div>
   );
--- component/TodoApp.tsx
+++ component/TodoApp.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Todo from "./Todo";
 
 /**
@@ -12,13 +12,38 @@ type Todos = {
 
 function App() {
   const [input, setInput] = useState<string>("");
-  const [todos, setTodos] = useState<Todos[]>([]);
+
+  const [todos, setTodos] = useState<Todos[]>(() => {
+    const saved = localStorage.getItem("todos");
+    if (saved) {
+      try {
+        const parsed: Todos[] = JSON.parse(saved);
+        console.log("from local storage", parsed);
+        return parsed;
+      } catch (e) {
+        console.log("error", e);
+        return [];
+      }
+    }
+    return [];
+  });
+
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  useEffect(() => {
+    if (isInitialized) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+      console.log("from local", todos);
+    } else {
+      setIsInitialized(true);
+    }
+  }, [todos, isInitialized]);
 
   const AddTodo = () => {
     if (!input.trim()) return;
     const newTodos = {
       id: Date.now(),
-      text: input,
+      text: input.trim(),
       completed: false,
     };
 
@@ -32,6 +57,14 @@ function App() {
         todo.id === id ? { ...todo, completed: true } : todo
       )
     );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key == "Enter") AddTodo();
   };
 
   return (
@@ -49,22 +82,37 @@ function App() {
             className="w-sm  border border-gray-500 rounded-xl bg-gray-700 placeholder-gray-400 p-3 text-white focus-within:border-b-white"
             type="text"
             placeholder="Write next task"
+            onKeyDown={handleKeyPress}
           />
           <button
             className="bg-orange-400 p-6 rounded-full cursor-pointer hover:bg-orange-600"
             onClick={AddTodo}
+            disabled={!input.trim()}
           >
             ➕
           </button>
         </div>
 
         <div className="container max-w-[500px]">
-          <h1 className="text-white font-bold text-center">Todos</h1>
-          {todos.map((todo) => {
-            return (
-              <Todo key={todo.id} todo={todo} completeTodo={completeTodo} />
-            );
-          })}
+          <h1 className="text-white font-bold text-center text-3xl">Todos</h1>
+          {todos.length > 0 ? (
+            <>
+              {todos.map((todo) => {
+                return (
+                  <Todo
+                    key={todo.id}
+                    todo={todo}
+                    completeTodo={completeTodo}
+                    deleteTodo={deleteTodo}
+                  />
+                );
+              })}
+            </>
+          ) : (
+            <h1 className="text-white font-bold text-6xl m-6 flex justify-center">
+              No Tasks
+            </h1>
+          )}
         </div>
       </div>
     </>
