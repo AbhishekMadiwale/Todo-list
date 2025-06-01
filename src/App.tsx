72d3727ae59040fc2fcf7b9e7b42ca07e48db0ad
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-06-01 11:23:24 +0530

    structured files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-
 import './App.css'
+import TodoApp from "../component/TodoApp"
 
 function App() {
 
   return (
     <>
-      <div className='bg-black min-h-screen p-10 flex justify-center  item-baseline'>
-        <div className='max-w-[500px] w-[90%] bg-transparent p-4 border-2 border-amber-50 h-24 rounded-xl align-top '>
-          <h1 className='text-white text-center'>Todo for the day</h1>
-        </div>
-
-      </div>
+      <TodoApp/>
     </>
   )
 }
