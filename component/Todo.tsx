47fb3e08a2fccfb2dcdf83d9f0565b8bc1bbff73
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-06-01 13:40:46 +0530

    Some cosmetic changes done

--- component/Todo.tsx
+++ component/Todo.tsx
@@ -14,13 +14,13 @@ type TodoProp = {
 function Todo({ todo, completeTodo, deleteTodo }: TodoProp) {
   return (
     <div className="bg-gray-700 text-white font-bold p-5 my-5 rounded-xl flex justify-between items-center">
-      <p className={todo.completed === true ? "line-through" : " "}>
+      <p className={todo.completed === true ? "line-through decoration-red-800 decoration-4" : " "}>
         {todo.text}
       </p>
       <div className="flex gap-10 items-center cursor-pointer">
         <FaCheckCircle
-          className="hover:text-green-800"
-          onClick={() => completeTodo(todo.id)}
+          className={todo.completed === true ? "text-green-800 cursor-not-allowed" :"hover:text-green-800"}
+          onClick={todo.completed ? undefined : () => completeTodo(todo.id)}
         />
         <FaTrash className="hover:text-red-600"
           onClick={() => deleteTodo(todo.id)}
